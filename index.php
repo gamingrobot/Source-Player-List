d281c5724843861a0031087bfd1e92baf93a084b
Gambit Roberts <gamingrobot@gmail.com>
2012-04-24 01:56:18 -0600

    back button works, refresh button, bookmarking works

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
 	<link rel="stylesheet" href="bootstrap.min.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="tinydropdown.js"></script>
-    <script type="text/javascript" src="jquery.tablesorter.js"></script>
+    <script type="text/javascript" src="jquery.tablesorter.min.js"></script>
+    <script type="text/javascript" src="jquery.ba-hashchange.min.js"></script>
+    
 </head>
 
 
 <body>
     <div class="container">
-        <h1>Player List</h1>
-    
+        <!--<h1>This is a testbed for player list find the full version <a href="http://playerlist.gamingrobot.net">here</a></h1>
+        --><h1>Player List</h1>
       <div class="row">
 				<table class="bordered-table zebra-striped">
 					<thead>
@@ -21,32 +23,33 @@
                     <th style="text-align:center" id="kalt">
                     <li><span class="main">CS:S</span>
                         <ul class="submenu" onclick="this.style.display='none'; return false;">
-                            <li id="css_dust2"><span>24/7 =(eGO)= DUST2</span></li>
-                            <li id="css_office"><span>24/7 =(eGO)= OFFICE</span></li>
-                            <li id="css_italy"><span>24/7 =(eGO)= ITALY</span></li>
-                            <li id="css_dedust"><span>24/7 =(eGO)= DE_DUST</span></li>
-                            <li id="css_highschool"><span>24/7 =(eGO)= HIGHSCHOOL</span></li>
+                            <li id="css_dust2"      onclick="window.location.hash='css_dust2';"><span>24/7 =(eGO)= DUST2</span></li>
+                            <li id="css_office"     onclick="window.location.hash='css_office';"><span>24/7 =(eGO)= OFFICE</span></li>
+                            <li id="css_italy"      onclick="window.location.hash='css_italy';"><span>24/7 =(eGO)= ITALY</span></li>
+                            <li id="css_dedust"     onclick="window.location.hash='css_dedust';"><span>24/7 =(eGO)= DE_DUST</span></li>
+                            <li id="css_highschool" onclick="window.location.hash='css_highschool';"><span>24/7 =(eGO)= HIGHSCHOOL</span></li>
                         </ul>
                     </li>
                     </th>
                     <th style="text-align:center" id="strand">
                     <li><span class="main">DOD:S</span>
                         <ul class="submenu" onclick="this.style.display='none'; return false;">
-                            <li id="dod_anzio"><span>24/7 =(eGO)= ANZIO</span></li>
-                            <li id="dod_kalt"><span>24/7 =(eGO)= KALT</span></li>
-                            <li id="dod_strand"><span>24/7 =(eGO)= CHARLIE AKA STRAND</span></li>
-                            <li id="dod_ava"><span>24/7 =(eGO)= AVALANCHE</span></li>
-                            <li id="dod_donner"><span>24/7 =(eGO)= DONNER</span></li>
+                            <li id="dod_anzio"  onclick="window.location.hash='dod_anzio';"><span>24/7 =(eGO)= ANZIO</span></li>
+                            <li id="dod_kalt"   onclick="window.location.hash='dod_kalt';"><span>24/7 =(eGO)= KALT</span></li>
+                            <li id="dod_strand" onclick="window.location.hash='dod_strand';"><span>24/7 =(eGO)= CHARLIE AKA STRAND</span></li>
+                            <li id="dod_ava"    onclick="window.location.hash='dod_ava';"><span>24/7 =(eGO)= AVALANCHE</span></li>
+                            <li id="dod_donner" onclick="window.location.hash='dod_donner';"><span>24/7 =(eGO)= DONNER</span></li>
                         </ul>
                     </li>
                     </th>
                     <th style="text-align:center" id="ava">
                     <li><span class="main">TF2</span>
                         <ul class="submenu" onclick="this.style.display='none'; return false;">
-                            <li id="tf2_2fort"><span>24/7 =(eGO)= 2FORT</span></li>
-                            <li id="tf2_valve"><span>24/7 =(eGO)= VALVE MAPS</span></li>
-                            <li id="tf2_foundry"><span>24/7 =(eGO)= FOUNDRY</span></li>
-                            <li id="tf2_dust"><span>24/7 =(eGO)= DUSTBOWL</span></li>
+                            <li id="tf2_2fort"      onclick="window.location.hash='tf2_2fort';"><span>24/7 =(eGO)= 2FORT</span></li>
+                            <li id="tf2_valve"      onclick="window.location.hash='tf2_valve';"><span>24/7 =(eGO)= VALVE MAPS</span></li>
+                            <li id="tf2_foundry"    onclick="window.location.hash='tf2_foundry';"><span>24/7 =(eGO)= FOUNDRY</span></li>
+                            <li id="tf2_dust"       onclick="window.location.hash='tf2_dust';"><span>24/7 =(eGO)= DUSTBOWL</span></li>
+                            <li id="tf2_black"      onclick="window.location.hash='tf2_black';"><span>24/7 =(eGO)= BLACKMESA</span></li>
                         </ul>
                     </li>
                     </th>
@@ -55,18 +58,18 @@
 				</table>
 		</div>
         <div class="row" id="welcome">
-            <table class="bordered-table zebra-striped" id="data_table">
+            <table class="bordered-table zebra-striped">
                     <thead>
                     <tr>
                         <th><span id="welcome_message">Use the menu above to select a server to view players on the server</th>
                     </tr>
-                </table>
+            </table>
         </div>
 		<div style="display:none" class="row" id="player_div">
 				<table class="bordered-table zebra-striped" id="data_table">
 					<thead id="player_head">
                     <tr>
-				        <th colspan="5" style="border-bottom:1px solid #DDD"><span id="servername"></span> <a style="float:right" href="#" >Refresh </a></th>
+				        <th colspan="5" style="border-bottom:1px solid #DDD"><span id="servername"></span> <a style="float:right; cursor: pointer" onclick="$(window).hashchange();">Refresh</a></th>
                     </tr>
 					<tr id="player_titles">
 						<th>Name</th>
@@ -83,59 +86,59 @@
 	</div>
     
     <script type="text/javascript">
+    var serverport = "27015";
+    var apiurl = "gaming_gameme_api.php"
+    var servers = new Array();
+    //CSS SERVERS
+    servers['css_dust2']        = '174.36.42.204';
+    servers['css_office']       = '174.36.42.205';
+    servers['css_italy']        = '75.126.144.83';
+    servers['css_dedust']       = '75.126.144.82';
+    servers['css_highschool']   = '75.126.144.80';
+    //DOD SERVERS
+    servers['dod_anzio']        = '72.233.123.6';
+    servers['dod_kalt']         = '72.233.123.5';
+    servers['dod_strand']       = '72.233.123.3';
+    servers['dod_ava']          = '67.228.175.242';
+    servers['dod_donner']       = '67.228.175.243';
+    //TF2 SERVERS
+    servers['tf2_2fort']        = '69.64.95.111';
+    servers['tf2_valve']        = '64.150.183.207';
+    servers['tf2_foundry']      = '68.232.161.203';
+    servers['tf2_dust']         = '64.150.186.180';
+    servers['tf2_black']        = '68.232.160.125';
+
+
+    //do page load stuff
+    //if(window.location.hash) {
+    //    var hashserver = window.location.hash.substring(1);
+    //    loadplayers(apiurl + "?ip=" + servers[hashserver] + ":" + serverport, hashserver);
+    //} 
+
+    $(window).hashchange( function(){
+        if(window.location.hash) {
+            var hash = location.hash;       
+            // Set the page title based on the hash.
+            hashserver = hash.replace( /^#/, '' );
+            loadplayers(apiurl + "?ip=" + servers[hashserver] + ":" + serverport, hashserver);
+        }
+        else {
+
+            $("#player_div").hide();
+            $("#welcome").show();
+        }
+        
+    })
+    $(window).hashchange();
+
     $.ajaxSetup ({
 		cache: false
 	});
     var players;
     var serverinfo;
     var tablebackup;
-    //CSS SERVERS
-    $("#css_dust2").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=174.36.42.204:27015");
-    });
-    $("#css_office").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=174.36.42.205:27015");
-    });
-    $("#css_italy").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=75.126.144.83:27015");
-    });
-    $("#css_dedust").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=75.126.144.82:27015");
-    });
-    $("#css_highschool").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=75.126.144.80:27015");
-    });
-    //DOD SERVERS
-    $("#dod_anzio").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=72.233.123.6:27015");
-    });
-    $("#dod_kalt").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=72.233.123.5:27015");
-    });
-    $("#dod_strand").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=72.233.123.3:27015");
-    });
-    $("#dod_ava").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=67.228.175.242:27015");
-    });
-    $("#dod_donner").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=67.228.175.243:27015");
-    });
-    //TF2 SERVERS
-    $("#tf2_2fort").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=69.64.95.111:27015");
-    });
-    $("#tf2_valve").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=64.150.183.207:27015");
-    });
-    $("#tf2_foundry").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=68.232.161.203:27015");
-    });
-    $("#tf2_dust").click(function(){
-        loadplayers("gaming_gameme_api.php?ip=64.150.186.180:27015");
-    });
-    function loadplayers(loadURL){
-        //$("#data_table").trigger("sorton", [[0,0]]);
+    function loadplayers(loadURL, servertag){
+        //window.location.hash = servertag;
         $("#player_div").show();
         $("#player_head").hide();
         $("#welcome").hide();
@@ -185,6 +188,7 @@
     <script type="text/javascript">
     //when updating the table call this function to clear the tablesorter data
     function resetTable() {
+      $("#data_table").hide();
       tablebackup.clone().insertAfter("#data_table");
       $("#data_table").remove();
       $.tablesorter.defaults.sortList = [[0,0]]; 
